758ebf94e2ebf3e3ef8d2276b0368682a8808400
sahanpramudith <pramuditsahan466@gmail.com>
2024-08-30 15:02:17 +0530

    add customer from

---
+++ src/main/java/controller/AddCustomerForm.java
@@ -0,0 +1,54 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class AddCustomerForm {
+
+    @FXML
+    private DatePicker DateDiob;
+
+    @FXML
+    private JFXComboBox<?> cmdTitle;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableView<?> tblTable;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtSalary;
+
+    @FXML
+    void btnOnActionAdd(ActionEvent event) {
+
+    }
+
+}
