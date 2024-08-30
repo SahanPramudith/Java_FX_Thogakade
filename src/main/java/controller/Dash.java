d56a3e3f1281f2e1430ba2bb717c911fab1165d4
sahanpramudith <pramuditsahan466@gmail.com>
2024-08-30 15:01:40 +0530

    Navigate

--- src/main/java/controller/Dash.java
+++ src/main/java/controller/Dash.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Dash {
+
+    @FXML
+    void btnOnActionAddCustomer(ActionEvent event) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/add_customer_form.fxml"))));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @FXML
+    void btnOnActionViewCustomer(ActionEvent event) {
+
+    }
+
 }
