56cfad504a0cc73706d90a85de5c3bf66dbb552f
sahanpramudith <pramuditsahan466@gmail.com>
2024-08-30 17:28:27 +0530

    Added data to array list

--- src/main/java/controller/AddCustomerForm.java
+++ src/main/java/controller/AddCustomerForm.java
@@ -2,19 +2,27 @@ package controller;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import model.Customer;
 
-public class AddCustomerForm {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class AddCustomerForm implements Initializable {
 
     @FXML
     private DatePicker DateDiob;
 
     @FXML
-    private JFXComboBox<?> cmdTitle;
+    private JFXComboBox<String> cmdTitle;
 
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -46,13 +54,38 @@ public class AddCustomerForm {
     @FXML
     private JFXTextField txtSalary;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> title = FXCollections.observableArrayList();
+        title.add("MR.");
+        title.add("Miss.");
+
+        cmdTitle.setItems(title);
+    }
+
+
+    ArrayList<Customer> customerList = new ArrayList<>();
     @FXML
     void btnOnActionAdd(ActionEvent event) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
+        String title = cmdTitle.getValue();
+
+        Customer customer = new Customer(id, name, address, salary, title);
+        customerList.add(customer);
+        System.out.println("customer = " + customer);
+
 
 
 
     }
 
     public void btnOnActionReloard(ActionEvent actionEvent) {
+
+
     }
+
+
 }
--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,12 +1,19 @@
 package model;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Customer {
     private String id;
     private String name;
     private String address;
-    private String salary;
-    private LocalDate dob;
+    private Double salary;
     private String title;
 }
